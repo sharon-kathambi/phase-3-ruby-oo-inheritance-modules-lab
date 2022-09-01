58a9a29d5b1c9777198858721de677fce697aa39
Sharon Mwenda <smkathambi@gmail.com>
2022-09-01 18:38:14 +0300

    commit tree 15569357e392937ec65891cb2e5c4b67bbf7923a

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,8 @@
 require 'pry'
 
 class Song
+  extend Memorable
+  #extend Findable
   attr_accessor :name
   attr_reader :artist
 
@@ -11,20 +13,20 @@ class Song
   end
 
   def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
+   @@songs.detect{|a| a.name == name}
   end
 
   def self.all
     @@songs
   end
 
-  def self.reset_all
-    self.all.clear
-  end
+ # def self.reset_all
+   # self.all.clear
+ # end
 
-  def self.count
-    self.all.count
-  end
+  #def self.count
+  #  self.all.count
+  #end
 
   def artist=(artist)
     @artist = artist
